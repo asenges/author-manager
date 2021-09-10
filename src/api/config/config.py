4a84c6920f4c5160e16c78f91964b69933762fd3
Alex Senges <asenges400@gmail.com>
2021-09-10 13:16:41 -0400

    SecurityCommit

--- src/api/config/config.py
+++ src/api/config/config.py
@@ -4,48 +4,46 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:8xr.t9iz99.xk.8XR@localhost:3306/bookstore_prod'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<user>:<pass>@localhost:3306/bookstore_prod'
     SQLALCHEMY_ECHO = False
     JWT_SECRET_KEY = 'JWT-SECRET'
     SECRET_KEY= 'SECRET-KEY'
     SECURITY_PASSWORD_SALT= 'SECRET-KEY-PASSWORD'
-    MAIL_DEFAULT_SENDER= 'asenges400@gmail.com'
+    MAIL_DEFAULT_SENDER= 'yourmail@gmail.com'
     MAIL_SERVER= 'smtp.gmail.com'
     MAIL_PORT= 465
-    MAIL_USERNAME= 'asenges400@gmail.com'
-    MAIL_PASSWORD= 'gjoczqpulezsgrsr'
+    MAIL_USERNAME= 'yourmail@gmail.com'
+    MAIL_PASSWORD= 'your-app-pass'
     MAIL_USE_TLS= False
     MAIL_USE_SSL= True
     UPLOAD_FOLDER= 'images'
 
 class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:8xr.t9iz99.xk.8XR@localhost:3306/bookstore_dev'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<user>:<pass>@localhost:3306/bookstore_prod'
     SQLALCHEMY_ECHO = False
     JWT_SECRET_KEY = 'JWT-SECRET'
     SECRET_KEY= 'SECRET-KEY'
     SECURITY_PASSWORD_SALT= 'SECRET-KEY-PASSWORD'
-    MAIL_DEFAULT_SENDER= 'asenges400@gmail.com'
+    MAIL_DEFAULT_SENDER= 'yourmail@gmail.com'
     MAIL_SERVER= 'smtp.gmail.com'
     MAIL_PORT= 465
-    MAIL_USERNAME= 'asenges400@gmail.com'
-    MAIL_PASSWORD= 'gjoczqpulezsgrsr'
+    MAIL_USERNAME= 'yourmail@gmail.com'
+    MAIL_PASSWORD= 'your-app-pass'
     MAIL_USE_TLS= False
     MAIL_USE_SSL= True
     UPLOAD_FOLDER= 'images'
 
 class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:8xr.t9iz99.xk.8XR@localhost:3306/bookstore_test'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<user>:<pass>@localhost:3306/bookstore_prod'
     SQLALCHEMY_ECHO = False
     JWT_SECRET_KEY = 'JWT-SECRET'
     SECRET_KEY= 'SECRET-KEY'
     SECURITY_PASSWORD_SALT= 'SECRET-KEY-PASSWORD'
-    MAIL_DEFAULT_SENDER= 'asenges400@gmail.com'
+    MAIL_DEFAULT_SENDER= 'yourmail@gmail.com'
     MAIL_SERVER= 'smtp.gmail.com'
     MAIL_PORT= 465
-    MAIL_USERNAME= 'asenges400@gmail.com'
-    MAIL_PASSWORD= 'gjoczqpulezsgrsr'
+    MAIL_USERNAME= 'yourmail@gmail.com'
+    MAIL_PASSWORD= 'your-app-pass'
     MAIL_USE_TLS= False
     MAIL_USE_SSL= True
     UPLOAD_FOLDER= 'images'
